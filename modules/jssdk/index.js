459c0261e1e01d56968c3561c5ae77e4f5f9cf78
xiongyongsheng <xiongys@humming-tech.com>
2023-11-14 16:49:35 +0800

    update: add wechat javascript bridge

--- modules/jssdk/index.js
+++ modules/jssdk/index.js
@@ -6,6 +6,7 @@ export function init() {
     debug: false,
     appId: this.options.appId,
     jsApiList: [],
+    openTagList: [],
   };
 }
 export async function signature(options) {
@@ -27,6 +28,7 @@ export async function signature(options) {
       nonceStr: this.JS_SDK_CONFIG.nonceStr, // 必填，生成签名的随机串
       signature: this.JS_SDK_CONFIG.signature, // 必填，签名
       jsApiList: this.JS_SDK_CONFIG.jsApiList, // 必填，需要使用的JS接口列表
+      openTagList: this.JS_SDK_CONFIG.openTagList, // 选填，需要使用的开放标签列表
     });
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
     wx.ready(res);
